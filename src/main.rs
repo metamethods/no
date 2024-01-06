d165859a3d6b95fc1e4c1dfad5b5ae3961f4b565
metamethods <tabledmetamethods@gmail.com>
2024-01-06 22:12:25 +0800

    Added 2 new flags!111

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,42 @@ struct Args {
 
   /// Wait x milliseconds between each line.
   #[arg(short, long)]
-  interval: Option<u64>
+  interval: Option<u64>,
+
+  /// Yes
+  #[arg(short, long)]
+  yes: bool,
+
+  /// Count
+  #[arg(short, long)]
+  count: Option<u32>
 }
 
 fn main() {
-  let string = Args::parse().strings
+  let args = Args::parse();
+  let string = args.strings
     .unwrap_or(vec!["n".to_string()])
     .join(" ");
 
-  let interval = Args::parse().interval;
+  let mut current_count: u32 = 0;
 
   loop {
-    println!("{string}");
+    current_count += 1;
 
-    if let Some(interval) = interval {
+    if args.yes {
+      println!("y");
+    } else {
+      println!("{string}");
+    }
+
+    if let Some(interval) = args.interval {
       thread::sleep(time::Duration::from_millis(interval));
+    }
+
+    if let Some(max_count) = args.count {
+      if current_count >= max_count  {
+        break;
+      }
     }
   }
 }
